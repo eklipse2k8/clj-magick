129f84a10e4187cc81544a35d08d909ac2da3527
Matt Jarjoura <matt@artcator.com>
2014-09-17 19:38:57 -0700

    Initial import with clojure wrapper

--- README.md
+++ README.md
@@ -1,4 +1,14 @@
-clj-magick
-==========
+# clj-magick2
 
-Clojure library for interfacing with ImageMagick/GraphicsMagick
+A Clojure library designed to ... well, that part is up to you.
+
+## Usage
+
+FIXME
+
+## License
+
+Copyright © 2014 FIXME
+
+Distributed under the Eclipse Public License either version 1.0 or (at
+your option) any later version.
---
+++ project.clj
@@ -0,0 +1,15 @@
+(defproject clj-magick "0.1.0-SNAPSHOT"
+  :description "Clojure library for interfacing with ImageMagick/GraphicsMagick"
+  :url "https://github.com/eklipse2k8/clj-magick"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.6.0"]]
+  
+  
+  :source-paths ["src/main/clojure"]
+  :java-source-paths ["src/main/java"]
+  :resource-paths ["src/main/resources"]
+  :test-paths ["src/test/clojure"]
+  
+  
+  )
